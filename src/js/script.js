0a068af68855ff941f8cdbc2f1cfc1aff86c8af8
Dima <zyumalin@bk.ru>
2020-06-15 16:31:31 +0300

    Test_1

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+// const menuBtn = document.getElementById('menuBtn');
+// const menuContainer = document.getElementById('menuContainer');
+
+// const menuIconClosed = "header-hamburger header-hamburger_closed"; //class name for closed button
+// const menuIconOpened = "header-hamburger header-hamburger_opened"; //class name for opened button
+// const menuContClosed = "header__menu header__menu_closed"; //class name for closed menu
+// const menuContOpened = "header__menu header__menu_opened"; //class name for opened menu
+
+// menuBtn.onclick = function() {
+//   if (menuBtn.className == menuIconClosed) {
+//     menuBtn.className = menuIconOpened;
+//     menuContainer.className = menuContOpened;
+//   } else if (menuBtn.className == menuIconOpened) {
+//     menuBtn.className = menuIconClosed;
+//     menuContainer.className = menuContClosed;
+//   }
+// }
